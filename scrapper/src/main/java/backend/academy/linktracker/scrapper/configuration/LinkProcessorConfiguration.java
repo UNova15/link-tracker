c4f2c31e232dd3c6f56b993f5133924ebb99c28e
Kirill <kkrecetov5@gmail.com>
2026-06-04 19:30:11 +0700

    fix bugs

--- scrapper/src/main/java/backend/academy/linktracker/scrapper/configuration/LinkProcessorConfiguration.java
+++ scrapper/src/main/java/backend/academy/linktracker/scrapper/configuration/LinkProcessorConfiguration.java
@@ -3,6 +3,7 @@ package backend.academy.linktracker.scrapper.configuration;
 import backend.academy.linktracker.scrapper.domain.LinkType;
 import backend.academy.linktracker.scrapper.linktracker.LinkProcessor;
 import backend.academy.linktracker.scrapper.linktracker.linkchecker.UpdateHandler;
+import backend.academy.linktracker.scrapper.mapper.NotificationMapper;
 import backend.academy.linktracker.scrapper.properties.ScrapperProperties;
 import backend.academy.linktracker.scrapper.repository.SubscriptionRepository;
 import java.util.List;
@@ -20,12 +21,14 @@ public class LinkProcessorConfiguration {
     public LinkProcessor linkProcessor(
             List<UpdateHandler> allCheckers,
             SubscriptionRepository subscriptionRepository,
+            NotificationMapper mapper,
             ExecutorService executorService,
             ScrapperProperties properties) {
         Map<LinkType, UpdateHandler> checkers =
                 allCheckers.stream().collect(Collectors.toMap(UpdateHandler::getLinkType, Function.identity()));
 
-        return new LinkProcessor(subscriptionRepository, checkers, executorService, properties.numberOfThreads());
+        return new LinkProcessor(
+                subscriptionRepository, checkers, mapper, executorService, properties.numberOfThreads());
     }
 
     @Bean
